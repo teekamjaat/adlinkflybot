4a7e956b9555108cb4880b68d19f130edeef299a
teekamjaat <160526899+teekamjaat@users.noreply.github.com>
2025-02-26 15:24:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,17 +50,12 @@ bot.onText(/\/start/, (msg) => {
 
   const username = msg.from.username;
 
-  const welcomeMessage = `Hello, ${username}!\n\n`
-
-    + 'Welcome to the URL Shortener Bot!\n'
-
-    + 'You can use this bot to shorten URLs using the mybios.eu.org service.\n\n'
-
-    + 'To shorten a URL, just type or paste the URL directly in the chat, and the bot will provide you with the shortened URL.\n\n'
-
-    + 'If you haven\'t set your MyBios API token yet, use the command:\n/api YOUR_MYBIOS_API_TOKEN\n\n'
-
-    + 'Now, go ahead and try it out!';
+  const welcomeMessage = `<b>Hello, ${username}!</b>\n\n`
+    + `<b>Welcome to the IndiaEarnX URL Shortener Bot!</b>\n`
+    + `<b>You can use this bot to shorten URLs using the indiaearnx.com API service.</b>\n\n`
+    + `<b>To shorten a URL, just type or paste the URL directly in the chat, and the bot will provide you with the shortened URL.</b>\n\n`
+    + `<b>If you haven't set your IndiaEarnX API token yet, use the command:</b>\n<code>/setapi YOUR_IndiaEarnx_API_TOKEN</code>\n\n`
+    + `<b>Example:</b>\n<code>/setapi c49399f821fc020161bc2a31475ec59f35ae5b4</code>`;
 
 
 
@@ -74,7 +69,7 @@ bot.onText(/\/start/, (msg) => {
 
 // Command: /api
 
-bot.onText(/\/api (.+)/, (msg, match) => {
+bot.onText(/\/setapi (.+)/, (msg, match) => {
 
   const chatId = msg.chat.id;
 
@@ -88,7 +83,7 @@ bot.onText(/\/api (.+)/, (msg, match) => {
 
 
 
-  const response = `MyBios API token set successfully. Your token: ${userToken}`;
+  const response = `IndiaEarnX API token set successfully. Your token: ${userToken}`;
 
   bot.sendMessage(chatId, response);
 
@@ -130,7 +125,7 @@ async function shortenUrlAndSend(chatId, Url) {
 
   if (!arklinksToken) {
 
-    bot.sendMessage(chatId, 'Please provide your MyBios API token first. Use the command: /api YOUR_MYBIOS_API_TOKEN');
+    bot.sendMessage(chatId, 'Please provide your IndiaEarnX API token first. Use the command: /api YOUR_IndiaEarnX_API_TOKEN');
 
     return;
 
@@ -140,7 +135,7 @@ async function shortenUrlAndSend(chatId, Url) {
 
   try {
 
-    const apiUrl = `https://mybios.eu.org/api?api=${arklinksToken}&url=${Url}`;
+    const apiUrl = `https://indiaearnx.com/api?api=${arklinksToken}&url=${Url}`;
 
 
 
